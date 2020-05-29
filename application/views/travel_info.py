8d58e233f25053aa6fc85d45994c2ee1a68df63f
Kong <khtks@naver.com>
2020-05-29 14:02:46 +0900

    server name 변경

--- application/views/travel_info.py
+++ application/views/travel_info.py
@@ -9,46 +9,45 @@ from application.schemata.travel_info import TravelInfoSchema
 from flask import Blueprint, request, Response, make_response, render_template, url_for, current_app
 from flask_restful import Resource
 
+import boto3
+from boto3.dynamodb.conditions import Key, Attr
+
 travel_info_bp = Blueprint("info", __name__, url_prefix='/info')
 info_schema = TravelInfoSchema()
 session = db.session
 api = api(travel_info_bp)
+
+dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+TableInformation = dynamodb.Table('ChatBotForInformation')
+TableSearch = dynamodb.Table('ChatBotForSearch')
 
 
 class Info(Resource):
     def get(self, contentID=None):
         if contentID is None:
             return make_response(render_template('display_result.html', title='no data', addr='no data', phone_number='no data', overview='no data', image='no data'), 200)
-        result = TravelInfo.query.get(contentID)
 
-        title = result.get_title()
-        addr = result.get_addr()
-        phone_number = result.get_phone_number()
-        img = result.get_img()
+        result = TableInformation.get_item(Key={'contentID': contentID})
+        res = result.get('Item')
+
+        title = res.get('title')
+        addr = res.get('addr')
+        phone_number = res.get('tel')
+        img = res.get('firstImage')
         if img != 'No firstimage':
-            with tempfile.NamedTemporaryFile(mode="wb") as image:
-                response = requests.get(img, stream=True)
-                image.write(response.content)
-                image.close()
+            # with tempfile.NamedTemporaryFile(mode="wb") as image:
+            #     response = requests.get(img, stream=True)
+            #     image.write(response.content)
+            #     image.close()
+            pass
 
-            # import requests
-            #
-            # url = "http://craphound.com/images/1006884_2adf8fc7.jpg"
-            # response = requests.get(url)
-            # if response.status_code == 200:
-            #     with open("/Users/apple/Desktop/sample.jpg", 'wb') as f:
-            #         f.write(response.content)
-
-
-        overview = result.get_overview()
-        overview = [x.lstrip() + '.' for x in overview.split('.')][:-1]
-
-        print(result)
+        overview = img = res.get('overview')
+        # overview = [x.lstrip() + '.' for x in overview.split('.')][:-1]
 
         path = os.path.join('static', 'photo')
         full_filename = os.path.join(path, 'sample_image.png')
         return make_response(render_template('display_result.html', title=title, addr=addr, phone_number=phone_number, overview=overview, image=full_filename), 200)
 
 
-api.add_resource(Info, '/<string:contentID>')
+api.add_resource(Info, '/<int:contentID>')
 
